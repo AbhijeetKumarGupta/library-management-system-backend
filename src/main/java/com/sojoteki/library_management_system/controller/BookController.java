3062483e1abb300bf97495f01c8de0d61a5b18fe
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2026-06-26 19:04:00 +0530

    feat: add get book by id controller and service

--- src/main/java/com/sojoteki/library_management_system/controller/BookController.java
+++ src/main/java/com/sojoteki/library_management_system/controller/BookController.java
@@ -3,10 +3,9 @@ package com.sojoteki.library_management_system.controller;
 import com.sojoteki.library_management_system.model.Book;
 import com.sojoteki.library_management_system.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/book")
@@ -18,5 +17,15 @@ public class BookController {
     @PostMapping("/save")
     public String saveDoctor(@RequestBody Book book){
         return bookService.saveBook(book);
+    }
+
+    @GetMapping("")
+    public List<Book> getAllBooks(){
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable int id){
+        return bookService.getBookById(id);
     }
 }
--- src/main/java/com/sojoteki/library_management_system/service/BookService.java
+++ src/main/java/com/sojoteki/library_management_system/service/BookService.java
@@ -5,6 +5,9 @@ import com.sojoteki.library_management_system.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class BookService {
 
@@ -14,5 +17,14 @@ public class BookService {
     public String saveBook(Book book){
         bookRepository.save(book);
         return "Book saved successfully!!";
+    }
+
+    public Book getBookById(int bookId){
+        Optional<Book> book = bookRepository.findById(bookId);
+        return book.orElse(null);
+    }
+
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
     }
 }
